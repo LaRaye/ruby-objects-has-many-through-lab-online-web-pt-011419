b85b84098c7974ed217e31cff0ab1f5251e93ad4
LaRaye Johnson <lnj2105@gmail.com>
2019-02-16 07:19:52 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,17 @@
+class Doctor
+  @@all = []
+  attr_accessor :name
+  
+  def initialize(name)
+    @name  = name 
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all
+  end
+  
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end 
+end 
